d8053032024dfcfc6b942ffa66a51d6568d04b4e
joshua clark <jclark@greenriver.edu>
2019-04-18 19:51:13 -0700

    awesome commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Because it is awesome
           </a>
         </header>
       </div>
